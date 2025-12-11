7fa54b2eb60568591d90356a0e36a27226d98720
Luís Fernando Nascimento <105133507+LuisFNascimento@users.noreply.github.com>
2025-12-11 11:53:34 -0300

    Implementado os pacotes persistence e gateway na infra

--- src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
+++ src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
@@ -2,18 +2,24 @@ package br.com.alura.codechella.infra.gateways;
 
 import br.com.alura.codechella.core.application.gateways.RepositorioDeUsuario;
 import br.com.alura.codechella.core.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.persistence.UsuarioRepository;
 
 import java.util.List;
 
 public class RepositorioDeUsuarioJpa implements RepositorioDeUsuario {
 
+    private final UsuarioRepository repository;
+
+    public RepositorioDeUsuarioJpa(UsuarioRepository repository) {
+        this.repository = repository;
+    }
     @Override
     public Usuario cadastrarUsuario(Usuario usuario) {
-        return null;
+        return repository.save(usuario);
     }
 
     @Override
     public List<Usuario> listarTodos() {
-        return List.of();
+        return repository.findAll();
     }
 }
--- src/main/java/br/com/alura/codechella/infra/persistence/UsuarioRepository.java
+++ src/main/java/br/com/alura/codechella/infra/persistence/UsuarioRepository.java
@@ -1,7 +1,6 @@
 package br.com.alura.codechella.infra.persistence;
 
-import br.com.alura.codechella.naoUsar.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 }
